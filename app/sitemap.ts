1d4276bad9d22365de87d603197983e31977aad7
visiondiba <naufalalthafadiba@gmail.com>
2026-03-23 11:24:39 +0700

    Fixing sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,9 +7,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString().split('T')[0], 
       changeFrequency: "monthly",
-      priority: 1,
+      priority: 1.0, 
     },
   ];
 }
