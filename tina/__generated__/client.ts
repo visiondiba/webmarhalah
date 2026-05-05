905e5ee217ab9bc53e38243e93b1c335e893833d
secretalyst2027 <secretalyst2027@gmail.com>
2026-05-05 23:32:13 +0700

    add

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/USER/Desktop/Secretalyst2027/webmarhalah/tina/__generated__/.cache/1777998548602', url: 'https://content.tinajs.io/2.3/content/03531803-f025-4070-bb2a-d140ce3b6747/github/main', token: '88a5501e215762d0f76c9fd0dc7a965fbf9051f0', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/USER/Desktop/Secretalyst2027/webmarhalah/tina/__generated__/.cache/1777998633155', url: 'https://content.tinajs.io/2.3/content/03531803-f025-4070-bb2a-d140ce3b6747/github/main', token: '88a5501e215762d0f76c9fd0dc7a965fbf9051f0', queries,  });
 export default client;
   
